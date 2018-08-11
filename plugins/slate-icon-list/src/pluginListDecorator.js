ab93390dd0c090b0ea1217402ec0d107f96b5218
杨坤海 <384037191@qq.com>
2018-08-11 15:18:41 +0800

    fix bug

--- plugins/slate-icon-list/src/pluginListDecorator.js
+++ plugins/slate-icon-list/src/pluginListDecorator.js
@@ -1,9 +1,10 @@
 // @flow
 import * as React from 'react';
 import type {IconProps} from 'shared/src/types';
-import blocklist, {DEFAULT} from '@canner/slate-helper-block-list';
 import EditList from 'slate-edit-list'
-import {OL_LIST, UL_LIST, LIST_ITEM} from '@canner/slate-constant/lib/blocks';
+import blocklist, {DEFAULT} from '../../slate-helper-block-list/src';
+
+import {OL_LIST, UL_LIST, LIST_ITEM} from '../../slate-constant/src/blocks';
 
 const {isList} = EditList(DEFAULT).utils;
 
